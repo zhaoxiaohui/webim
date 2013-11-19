61c8aae0ffeafb7bd39ce027b60a7b989f8451a7
zhaoxiaohui <fire.guy@163.com>
2013-11-19 20:57:04 +0800

    test

--- js/notify.js
+++ js/notify.js
@@ -1,5 +1,6 @@
 $(document).on("pageshow","#notify",function(event){
 	var notifys = Entity.getNotify();
+	$("#notify-list").empty();
 	if(notifys){
 		$.each(notifys, function(i, one){
 			//one = JSON.parse(one);
@@ -9,7 +10,7 @@ $(document).on("pageshow","#notify",function(event){
 				span= "<span class='ui-li-count ui-btn-up-b ui-btn-corner-all unread"+i+" unread'>"+1+"</span>";
 			var classes = "notify-"+i;
 			if(one.type == "addfriend"){
-				info = "好友添加申请...";
+				info = "<a href=#add-notify-info-content>好友添加申请...</a>";
 			}else{
 				info = "添加状态提醒...";
 			}
@@ -21,7 +22,7 @@ $(document).on("pageshow","#notify",function(event){
 				if(one.type == "addfriend"){
 					$("#add-notify-info-header").html("添加好友");
 					$("#add-notify-info-content").html(one.playboard.nickname + " 已经添加添加您为好友");
-					$.mobile.changePage("#add-notify-info", { role: "dialog" } );
+					//$.mobile.changePage("#add-notify-info", { role: "dialog" } );
 				}else{
 					$("#notify-info-header").html("添加好友-状态");
 					$("#notify-info-content").html("添加 "+one.playboard.nickname + one.playboard.confirm==true?" 成功":" 失败");
